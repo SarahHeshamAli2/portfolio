b32bb82f543f4af9fd8c4a4883a7ac4e2eb123d4
sarah hesham <sarah.h.nashaat@gmail.com>
2026-05-08 16:36:03 +0300

    (fix): fixing resend function on contact route

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -10,9 +10,9 @@ const client = createClient({
   useCdn: false,
 });
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
   try {
     const body = await req.json();
     const { name, email, subject, message } = body;
